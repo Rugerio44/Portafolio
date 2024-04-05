280abb01532ecc4b657046465896c368076fe332
Lezama44 <giannieljefe@hotmail.com>
2024-04-05 11:27:54 -0600

    arreglo del movimiento de los botones en mobile

--- assets/java/marquee.js
+++ assets/java/marquee.js
@@ -1,11 +1,17 @@
-document.addEventListener("DOMContentLoaded", (event) => {
 
+document.addEventListener("DOMContentLoaded", (event) => {
+    let intervalId = null;
     let marquee = (selector_list,speed) => {
         const list = document.querySelector(selector_list);
         let move = 0;
 
-        setInterval(() => {
-           list.style.marginLeft = `-${move}px` ;/*La comilla invertida es con Alt 96*/
+        // Si ya existe un intervalo, lo limpiamos
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
+
+        intervalId = setInterval(() => {
+           list.style.marginLeft = `-${move}px`; /*La comilla invertida es con Alt 96*/
            
            if(move > list.clientWidth){
                 move = 0;
@@ -14,8 +20,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
            move = move + speed;
         }, 0);
     }
-    
 
-    marquee(".marquee__list",0.3);
-    /*0.3*/ 
+    const updateMarqueeSpeed = () => {
+        if (window.innerWidth > 1060) {
+            marquee(".marquee__list",0.1);
+        } else {
+            marquee(".marquee__list",0.9);
+        }
+    }
+
+    // Ejecuta la función cuando se carga el DOM
+    updateMarqueeSpeed();
+
+    // Y también cada vez que se cambia el tamaño de la ventana
+    window.addEventListener('resize', updateMarqueeSpeed);
 });
+
